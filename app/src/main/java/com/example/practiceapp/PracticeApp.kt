90ee7fb7624a85fbf9c61a2013f5c4db7695e63c
cacao <cacao1562@naver.com>
2021-06-03 17:50:25 +0900

    first commit

---
+++ app/src/main/java/com/example/practiceapp/PracticeApp.kt
@@ -0,0 +1,13 @@
+package com.example.practiceapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PracticeApp : Application() {
+
+//    override fun onCreate() {
+//        super.onCreate()
+//        if (BuildConfig.DEBUG) Timber.plant(DebugTree())
+//    }
+}
