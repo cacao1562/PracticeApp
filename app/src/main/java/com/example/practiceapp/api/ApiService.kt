d5a50a630626024d58edbb9dc825f7d4fe249681
cacao <cacao1562@naver.com>
2021-06-10 16:24:52 +0900

    Add Network Module

---
+++ app/src/main/java/com/example/practiceapp/api/ApiService.kt
@@ -0,0 +1,4 @@
+package com.example.practiceapp.api
+
+interface ApiService {
+}
---
+++ app/src/main/java/com/example/practiceapp/di/NetworkModule.kt
@@ -0,0 +1,57 @@
+package com.example.practiceapp.di
+
+import com.example.practiceapp.BuildConfig
+import com.example.practiceapp.api.ApiService
+import com.example.practiceapp.util.BASE_URL
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val logging = HttpLoggingInterceptor()
+        if (BuildConfig.DEBUG) logging.level = HttpLoggingInterceptor.Level.BODY
+        val okHttpClient = OkHttpClient.Builder()
+        okHttpClient.interceptors().add(logging)
+        okHttpClient.interceptors().add(HttpLoggingInterceptor())
+        return okHttpClient.build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient, moshi: Moshi) : Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+//            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNewssterService(retrofit: Retrofit): ApiService{
+        return retrofit.create(ApiService::class.java)
+    }
+}
